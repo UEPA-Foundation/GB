1a26e133edf7f08d956b5e352d5c91dc1c31b2f3
Lucas Stankus <lucas.p.stankus@gmail.com>
2022-07-25 19:36:06 -0300

    gb: create gameboy skeleton structure
    Co-authored-by: rsgrava <rsgrava@hotmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 mod cpu;
+mod gameboy;
 
 extern crate num;
-#[macro_use]
 extern crate num_derive;
 
-#[allow(dead_code)]
-static CPU: cpu::Cpu = cpu::Cpu { a: 0, f: 0, b: 0, c: 0, d: 0, e: 0, h: 0, l: 0, sp: 0, pc: 0 };
-
-fn main() {}
+fn main() {
+    let mut gb = gameboy::GameBoy::init();
+    gb.fetch_exec();
+}
