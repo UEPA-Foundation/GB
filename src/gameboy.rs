4e9ad4effab86224d9928c86c835363124295304
Lucas Stankus <lucas.p.stankus@gmail.com>
2022-07-27 18:27:31 -0300

    gb: add interrupt master enable flag
    The logic will have to be further developed when we implement the EI
    instruction, however this is fine for reti (which will be implemented
    in the next commit)

--- src/gameboy.rs
+++ src/gameboy.rs
@@ -5,10 +5,11 @@ pub type Opcode = u8;
 pub struct GameBoy {
     pub cpu: cpu::Cpu,
     pub mem: [u8; 4096],
+    pub ime: bool,
 }
 
 impl GameBoy {
     pub fn init() -> Self {
-        Self { cpu: cpu::Cpu { a: 0, f: 0, b: 0, c: 0, d: 0, e: 0, h: 0, l: 0, sp: 0, pc: 0 }, mem: [0;4096] }
+        Self { cpu: cpu::Cpu { a: 0, f: 0, b: 0, c: 0, d: 0, e: 0, h: 0, l: 0, sp: 0, pc: 0 }, mem: [0;4096], ime: false }
     }
 }
