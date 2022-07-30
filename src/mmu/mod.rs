87193f1433ea0615b13ebfe40ce0ddffb2b67393
Lucas Stankus <lucas.p.stankus@gmail.com>
2022-07-30 04:41:02 -0300

    mmu: comment unused fields
    Having a bunch of known and meaningless errors distracts while trying to
    debug something we are currently working on. Fields will be uncommented
    when they become the development target.

--- src/mmu/mod.rs
+++ src/mmu/mod.rs
@@ -6,15 +6,15 @@ mod wramx;
 
 pub struct Mmu {
     cart: Box<dyn Cartridge>,
-    vram: VRam,
+    // vram: VRam,
     wram0: WRam0,
     wramx: WRamX,
-    echo: Echo,
-    oam: Oam,
-    unused: Unused,
-    io: IoRegisters,
-    hram: HRam,
-    ie: IeRegister,
+    // echo: Echo,
+    // oam: Oam,
+    // unused: Unused,
+    // io: IoRegisters,
+    // hram: HRam,
+    // ie: IeRegister,
 }
 
 pub trait MemoryUnit {
@@ -34,16 +34,17 @@ impl Mmu {
         match index {
             0x0000..=0x3FFF => self.cart.rom0_read(index),
             0x4000..=0x7FFF => self.cart.romx_read(index),
-            0x8000..=0x9FFF => self.vram.read(index),
+            // 0x8000..=0x9FFF => self.vram.read(index),
             0xA000..=0xBFFF => self.cart.sram_read(index),
             0xC000..=0xCFFF => self.wram0.read(index),
             0xD000..=0xDFFF => self.wramx.read(index),
-            0xE000..=0xFDFF => self.echo.read(index),
-            0xFE00..=0xFE9F => self.oam.read(index),
-            0xFFA0..=0xFEFF => self.unused.read(index),
-            0xFF00..=0xFF7F => self.io.read(index),
-            0xFF80..=0xFFFE => self.hram.read(index),
-            0xFFFF => self.ie.read(index),
+            // 0xE000..=0xFDFF => self.echo.read(index),
+            // 0xFE00..=0xFE9F => self.oam.read(index),
+            // 0xFFA0..=0xFEFF => self.unused.read(index),
+            // 0xFF00..=0xFF7F => self.io.read(index),
+            // 0xFF80..=0xFFFE => self.hram.read(index),
+            // 0xFFFF => self.ie.read(index),
+            _ => panic!(),
         }
     }
 
@@ -51,16 +52,17 @@ impl Mmu {
         match index {
             0x0000..=0x3FFF => self.cart.rom0_write(index, val),
             0x4000..=0x7FFF => self.cart.romx_write(index, val),
-            0x8000..=0x9FFF => self.vram.write(index, val),
+            // 0x8000..=0x9FFF => self.vram.write(index, val),
             0xA000..=0xBFFF => self.cart.sram_write(index, val),
             0xC000..=0xCFFF => self.wram0.write(index, val),
             0xD000..=0xDFFF => self.wramx.write(index, val),
-            0xE000..=0xFDFF => self.echo.write(index, val),
-            0xFE00..=0xFE9F => self.oam.write(index, val),
-            0xFFA0..=0xFEFF => self.unused.write(index, val),
-            0xFF00..=0xFF7F => self.io.write(index, val),
-            0xFF80..=0xFFFE => self.hram.write(index, val),
-            0xFFFF => self.ie.write(index, val),
+            // 0xE000..=0xFDFF => self.echo.write(index, val),
+            // 0xFE00..=0xFE9F => self.oam.write(index, val),
+            // 0xFFA0..=0xFEFF => self.unused.write(index, val),
+            // 0xFF00..=0xFF7F => self.io.write(index, val),
+            // 0xFF80..=0xFFFE => self.hram.write(index, val),
+            // 0xFFFF => self.ie.write(index, val),
+            _ => panic!(),
         }
     }
 }
