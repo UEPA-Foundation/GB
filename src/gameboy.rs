096f6145ded960f41ae8ebda9c1d8667b50003d7
Lucas Stankus <lucas.p.stankus@gmail.com>
2022-08-04 01:25:22 -0300

    gb: start CPU with pc at 0x100
    This skips boot procedure and starts directly at the cartridge loading
    point. This is a temporary workaround to run CPU test ROMs.
    
    Co-authored-by: rsgrava <rsgrava@hotmail.com>

--- src/gameboy.rs
+++ src/gameboy.rs
@@ -10,7 +10,7 @@ pub struct GameBoy {
 impl GameBoy {
     pub fn init(path: &str) -> Self {
         Self {
-            cpu: cpu::Cpu { a: 0, f: 0, b: 0, c: 0, d: 0, e: 0, h: 0, l: 0, sp: 0, pc: 0 },
+            cpu: cpu::Cpu { a: 0, f: 0, b: 0, c: 0, d: 0, e: 0, h: 0, l: 0, sp: 0, pc: 0x100 },
             mmu: mmu::Mmu::init(path),
             ime: false,
             enabling_int: false,
