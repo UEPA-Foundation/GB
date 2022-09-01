1654d8f68581484269c80b5ecf6cea1b9fab4179
rsgrava <rsgrava@hotmail.com>
2022-09-01 16:07:50 -0300

    pires meu amigo

--- src/mmu/ppu/fifo.rs
+++ src/mmu/ppu/fifo.rs
@@ -11,7 +11,8 @@ pub enum FifoState {
 pub struct PixelFifo {
     len: u8,
     flags: u8,
-    pixels: u16,
+    pixels_lo: u16,
+    pixels_lo: u16,
     pub state: FifoState,
 }
 
@@ -22,12 +23,15 @@ enum FifoError {
 }
 
 impl PixelFifo {
-    pub fn push(&mut self, pixel: u8) -> Result<(), FifoError> {
-        if self.len >= 16 {
+    pub fn push(&mut self, data_lo: u8, data_hi, num_pixels) -> Result<(), FifoError> {
+        if self.len >= 8 {
             Err(FifoError::Full)
         } else {
-            self.pixels |= ((pixel & 0b11) as u16) << self.len;
-            self.len += 2;
+            let num_pushed = std::cmp::min(num_pixels, 8 - self.len);
+            let mask = if num_pushed == 8 { 0xFF } else { (1 << num_pushed) - 1 };
+            self.pixels_lo |= data_lo & mask;
+            self.pixels_hi |= data_hi & mask;
+            self.len += num_pushed;
             Ok(())
         }
     }
@@ -36,8 +40,11 @@ impl PixelFifo {
         if self.len <= 0 {
             Err(FifoError::Empty)
         } else {
-            self.len -= 2;
-            Ok(((self.pixels >> self.len) & 0b11) as u8)
+            self.len -= 1;
+            let pixel = ((self.pixels_lo & 0x80) >> 7) | ((self.pixels_hi & 0x80) >> 6)
+            self.pixels_lo <<= 1;
+            self.pixels_hi <<= 1;
+            Ok(pixel)
         }
     }
 
@@ -45,7 +52,7 @@ impl PixelFifo {
         if self.len <= 0 {
             Err(FifoError::Empty)
         } else {
-            Ok(((self.pixels >> (self.len - 2)) & 0b11) as u8)
+            Ok(())
         }
     }
 
@@ -54,6 +61,8 @@ impl PixelFifo {
     }
 
     pub fn clear(&mut self) {
+        self.pixels_lo = 0;
+        self.pixels_hi = 0;
         self.len = 0;
         self.flags = 0;
     }
