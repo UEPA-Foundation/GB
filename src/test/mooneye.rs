6056204aa3c7afaad00ccc4812b7ee45f2bf7886
Lucas Stankus <lucas.p.stankus@gmail.com>
2022-09-10 15:54:12 -0300

    test: mooneye: add timer tests

--- src/test/mooneye.rs
+++ src/test/mooneye.rs
@@ -22,3 +22,17 @@ macro_rules! test_mooneye {
     };
 }
 test_mooneye!(daa, "acceptance/instr/daa.gb");
+
+test_mooneye!(timer_div_write, "acceptance/timer/div_write.gb");
+test_mooneye!(timer_rapid_toggle, "acceptance/timer/rapid_toggle.gb");
+test_mooneye!(timer_tim00, "acceptance/timer/tim00.gb");
+test_mooneye!(timer_tim00_div_trigger, "acceptance/timer/tim00_div_trigger.gb");
+test_mooneye!(timer_tim01, "acceptance/timer/tim01.gb");
+test_mooneye!(timer_tim01_div_trigger, "acceptance/timer/tim01_div_trigger.gb");
+test_mooneye!(timer_tim10, "acceptance/timer/tim10.gb");
+test_mooneye!(timer_tim10_div_trigger, "acceptance/timer/tim10_div_trigger.gb");
+test_mooneye!(timer_tim11, "acceptance/timer/tim11.gb");
+test_mooneye!(timer_tim11_div_trigger, "acceptance/timer/tim11_div_trigger.gb");
+test_mooneye!(timer_tima_reload, "acceptance/timer/tima_reload.gb");
+test_mooneye!(timer_tima_write_reloading, "acceptance/timer/tima_write_reloading.gb");
+test_mooneye!(timer_tma_write_reloading, "acceptance/timer/tma_write_reloading.gb");
