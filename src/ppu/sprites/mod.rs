50ebb391fd73715ad2ad40dcda65079a4de05c56
Lucas Stankus <lucas.p.stankus@gmail.com>
2022-09-28 18:27:22 -0300

    ppu: sprites: factor Fetcher struct into the Sprites struct

--- src/ppu/sprites/mod.rs
+++ src/ppu/sprites/mod.rs
@@ -4,19 +4,15 @@ mod fifo;
 
 pub struct Sprites {
     state: State,
-    obj: Object,
+
+    cur_obj: Object,
+    fetcher_idx: u8,
+    obj_buffer: Vec<Object>,
 
     data_lo: u8,
     data_hi: u8,
 
     fifo: Fifo,
-    fetcher: Fetcher,
-}
-
-struct Fetcher {
-    cur: u8,
-    buffer: [Object; 10],
-    len: u8,
 }
 
 #[derive(Clone, Copy)]
@@ -39,55 +35,54 @@ impl Sprites {
     pub fn init() -> Self {
         Self {
             state: State::SLEEP,
-            obj: Object { x: 0, y: 0, id: 0, flags: 0 },
+            cur_obj: Object { x: 0, y: 0, id: 0, flags: 0 },
+            fetcher_idx: 0,
+            obj_buffer: vec![Object { x: 0, y: 0, id: 0, flags: 0 }; 10],
             data_lo: 0,
             data_hi: 0,
             fifo: Fifo::init(),
-            fetcher: Fetcher { cur: 0, buffer: [Object { x: 0, y: 0, id: 0, flags: 0 }; 10], len: 0 },
         }
     }
 }
 
 impl super::Ppu {
     pub(super) fn fetch_object(&mut self) {
-        let obj_addr = 0xFE00 + (self.sp.fetcher.cur as u16 * 4);
+        let obj_addr = 0xFE00 + (self.sp.fetcher_idx as u16 * 4);
+        self.sp.fetcher_idx += 1;
+
         let obj = Object {
             y: self.oam.read(obj_addr + 0),
             x: self.oam.read(obj_addr + 1),
             id: self.oam.read(obj_addr + 2),
             flags: self.oam.read(obj_addr + 3),
         };
-        self.sp.fetcher.cur += 1;
 
         let obj_height = 8; // TODO: check tall sprite mode
         if (obj.x == 0) || (self.ly + 16 < obj.y) || (self.ly + 16 >= obj.y + obj_height) {
             return;
         }
 
-        let fetcher = &mut self.sp.fetcher;
-        if fetcher.len < 10 {
-            fetcher.buffer[fetcher.len as usize] = obj;
-            fetcher.len += 1;
+        if self.sp.obj_buffer.len() < 10 {
+            self.sp.obj_buffer.push(obj);
         }
     }
 
     pub(super) fn clear_sp_fetcher(&mut self) {
-        self.sp.fetcher.cur = 0;
-        self.sp.fetcher.len = 0;
-        self.sp.fetcher.buffer = [Object { x: 0, y: 0, id: 0, flags: 0 }; 10];
+        self.sp.fetcher_idx = 0;
+        self.sp.obj_buffer.clear();
     }
 
     pub(super) fn init_scanline_sp(&mut self) {
         self.sp.fifo.clear();
-        self.sp.state = if self.sp.fetcher.len == 0 { State::SLEEP } else { State::INDEX };
+        self.sp.state = if self.sp.obj_buffer.len() == 0 { State::SLEEP } else { State::INDEX };
     }
 
     pub(super) fn cycle_sp(&mut self) {
         match self.sp.state {
             State::INDEX => {
-                for obj in self.sp.fetcher.buffer[..self.sp.fetcher.len as usize].into_iter() {
+                for obj in &self.sp.obj_buffer {
                     if self.lx + 8 <= obj.x && obj.x < self.lx + 16 {
-                        self.sp.obj = *obj;
+                        self.sp.cur_obj = *obj;
                         self.sp.state = State::DATALOW;
                     }
                 }
@@ -101,12 +96,12 @@ impl super::Ppu {
                 self.sp.state = State::PUSH;
             }
             State::PUSH => {
-                let push_amnt = u8::min(u8::saturating_sub(self.sp.obj.x, 8), 8);
-                if self.sp.obj.flags & 0x20 != 0 {
+                let push_amnt = u8::min(u8::saturating_sub(self.sp.cur_obj.x, 8), 8);
+                if self.sp.cur_obj.flags & 0x20 != 0 {
                     self.sp.data_lo = mirror_byte(self.sp.data_lo);
                     self.sp.data_hi = mirror_byte(self.sp.data_hi);
                 }
-                self.sp.fifo.push(self.sp.data_lo, self.sp.data_hi, self.sp.obj.flags, push_amnt);
+                self.sp.fifo.push(self.sp.data_lo, self.sp.data_hi, self.sp.cur_obj.flags, push_amnt);
                 self.sp.state = State::INDEX;
             }
             State::SLEEP => {}
@@ -114,8 +109,8 @@ impl super::Ppu {
     }
 
     fn get_sprite_addr(&self) -> u16 {
-        let tile_addr = 0x8000 + (self.sp.obj.id as u16 * 16);
-        tile_addr + 2 * ((self.ly as u16 + 16 - self.sp.obj.y as u16) % 8)
+        let tile_addr = 0x8000 + (self.sp.cur_obj.id as u16 * 16);
+        tile_addr + 2 * ((self.ly as u16 + 16 - self.sp.cur_obj.y as u16) % 8)
     }
 
     #[inline(always)]
